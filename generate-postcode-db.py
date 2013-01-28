38653b32efd4a44c5cb770c937bb699a16697f5b
Rob Day <rkd@rkd.me.uk>
2013-01-28 19:37:09 +0000

    Correcting comments/line length

--- generate-postcode-db.py
+++ generate-postcode-db.py
@@ -14,7 +14,6 @@ from shapely.geometry import Point
 from ostn02python.eastings_to_decimal_degrees import postcodes_to_points
 from utils import get_constituency_list
 
-
 def guess_unknown_postcodes(unknowns, constituencies):
     postcode_constituencies = {}
     for postcode, point in unknowns:
@@ -37,10 +36,10 @@ def map_postcodes_to_constituencies(postcode_file, constituencies, verbose=False
         for postcode,point in postcodes_to_points(f):
             n += 1
             
-            # The postcodes are in alphabetical order - so it's relatively likely
-            # that this postcode will be in the same constituency as one of the
-            # last five postcodes. Check those first.
-            
+            # The postcodes are in alphabetical order - so it's relatively
+            # likely that this postcode will be in the same constituency as one
+            # of the last five postcodes.  Check those first.
+
             for guess in last_constituencies:
                 if constituencies[guess].contains(point):
                     last_constituencies.remove(guess)
@@ -57,10 +56,11 @@ def map_postcodes_to_constituencies(postcode_file, constituencies, verbose=False
                         break
 
             # Sometimes we don't get a result, usually because of postcodes on
-            # Scottish islands which fall outside the constituency boundaries we're
-            # using. Collect those postcodes into a list of unknowns, for further
-            # processing (eg. a lookup on http://parliament.uk or a best guess by
-            # distance).
+            # Scottish islands which fall outside the constituency boundaries
+            # we're using.  Collect those postcodes into a list of unknowns,
+            # for further processing (eg. a lookup on http://parliament.uk or a
+            # best guess by distance).
+
             if postcode not in postcode_constituencies:
                 unknowns.append((postcode, point))
             
@@ -71,11 +71,16 @@ def map_postcodes_to_constituencies(postcode_file, constituencies, verbose=False
 
 if __name__ == "__main__":
     constituencies = get_constituency_list("constituencies.kml")
-    (postcode_constituencies, unknowns) = map_postcodes_to_constituencies("all_postcodes.csv", constituencies)
-    postcode_constituencies.update(guess_unknown_postcodes(unknowns, constituencies))
+    (postcode_constituencies, unknowns) = map_postcodes_to_constituencies(
+            "all_postcodes.csv",
+            constituencies)
+    postcode_constituencies.update(guess_unknown_postcodes(unknowns,
+        constituencies))
     
     for postcode in postcode_constituencies:
-        this_constituency = waw_app.models.Constituency.objects.filter(name__exact=postcode_constituencies[postcode])[0]
-        new_postcode = waw_app.models.Postcode.objects.create(postcode=postcode, constituency=this_constituency)
+        this_constituency = waw_app.models.Constituency.objects.filter(
+                name__exact=postcode_constituencies[postcode])[0]
+        new_postcode = waw_app.models.Postcode.objects.create(
+                postcode=postcode, constituency=this_constituency)
         new_postcode.save()
 
